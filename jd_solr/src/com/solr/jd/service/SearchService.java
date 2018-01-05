deb7de8a4d2bc4780f68a73747fa73426977c5e5
hmy <hmy>
2018-01-05 20:52:04 +0800

    超时已修改

--- jd_solr/src/com/solr/jd/service/SearchService.java
+++
@@ -1,9 +0,0 @@
-package com.solr.jd.service;
-
-import com.solr.jd.pojo.Result;
-
-public interface SearchService {
-
-	Result queryProduct(String queryString, String catalog_name, String price, String sort, Integer page) throws Exception;
-
-}
